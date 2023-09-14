ecb26c5357973d52baf37d2e910f627fb7cd29bb
Eloi Charly <nandry556@gmail.com>
2023-09-14 16:42:32 +0200

    fix: begin work in update$

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -46,8 +46,18 @@ export const findById = async (
 // UPDATE  USER IN DATABASE
 export const updateUser = async (req: Request, res: Response) => {
   try {
-    const id = req.params.id;
-    res.send(`The id de user to update  ${id}`);
+    const _id = req.params.id;
+    const user = await User.findById(_id).exec();
+    if (user) {
+      // Sets `name` and unsets all other properties
+      await User.replaceOne({ _id }, { username: "Jean-Luc Picard" });
+      const user_to_update = await User.findById(_id).exec();
+      return res.status(200).json({ data: user_to_update });
+    } else {
+      return res
+        .status(500)
+        .json({ messages: "Aucun utilisateur trouvé avec cet ID." });
+    }
   } catch (error) {
     console.error("Erreur lors de la modification  du user:", error);
     return res.status(500).json({ error });
